007674390e8e867aef15d6d4519985434f66b631
Nilesh Puranik <nileshpuranik@live.com>
2023-10-25 22:15:13 +0000

    2 Fragments

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,68 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment) {
         "use strict";
 
         return Controller.extend("project2.controller.View1", {
-            onInit: function () {
-
+            onInit: function () {},
+            onBeforeRendering: function () {
+              debugger;
+            },
+            onVHMaterial: function () {},
+      
+            onVHPLant: function () {},
+      
+            onExecute: function () {
+              sap.m.MessageToast.show("On press of Execute");
+            },
+      
+            onButton1: function () {
+              var oView = this.getView();
+              if (!this.pDialog1) {
+                Fragment.load({
+                  id: oView.getId(),
+                  name: "project2.view.fragments.one",
+                  controller: this,
+                }).then(
+                  function (oDialog) {
+                    oView.addDependent(oDialog);
+                    this.pDialog1 = oDialog;
+                    oDialog.open();
+                  }.bind(this)
+                );
+              } else {
+                this.pDialog1.open();
+              }
+            },
+            onButton2: function () {
+              var oView = this.getView();
+              if (!this.pDialog2) {
+                Fragment.load({
+                  id: oView.getId(),
+                  name: "project2.view.fragments.two",
+                  controller: this,
+                }).then(
+                  function (oDialog) {
+                    oView.addDependent(oDialog);
+                    this.pDialog2 = oDialog;
+                    oDialog.open();
+                  }.bind(this)
+                );
+              } else {
+                this.pDialog2.open();
+              }
+            },
+      
+            onDlg1Close: function () {
+              this.pDialog1.close();
+            },
+            onDlg2Close: function () {
+              this.pDialog2.close();
             }
         });
     });
--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,23 +1,41 @@
-sap.ui.define([
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/Device"
-], 
-    /**
-     * provide app-view type models (as in the first "V" in MVVC)
-     * 
-     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
-     * @param {typeof sap.ui.Device} Device
-     * 
-     * @returns {Function} createDeviceModel() for providing runtime info for the device the UI5 app is running on
-     */
-    function (JSONModel, Device) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/model/json/JSONModel", "sap/ui/Device"],
+  /**
+   * provide app-view type models (as in the first "V" in MVVC)
+   *
+   * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+   * @param {typeof sap.ui.Device} Device
+   *
+   * @returns {Function} createDeviceModel() for providing runtime info for the device the UI5 app is running on
+   */
+  function (JSONModel, Device) {
+    "use strict";
 
-        return {
-            createDeviceModel: function () {
-                var oModel = new JSONModel(Device);
-                oModel.setDefaultBindingMode("OneWay");
-                return oModel;
-        }
+    return {
+      createDeviceModel: function () {
+        var oModel = new JSONModel(Device);
+        oModel.setDefaultBindingMode("OneWay");
+        return oModel;
+      },
+      createDummyModel: function () {
+        var oModel = new JSONModel({
+          entity1: {
+            FarmerId: "F123",
+            Material: "M333333",
+            Quantity: 10,
+            Date: "2022-03-24",
+            Plant: "DE20",
+          },
+          entity2: {
+            EmployeeNumber: "55555555",
+          },
+          entity3: {
+            VehicleName: "BMW",
+          },
+        });
+        oModel.setDefaultBindingMode("TwoWay");
+        return oModel;
+      },
     };
-});
+  }
+);
